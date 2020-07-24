8e1ce2358d8d06a6e1ba8d95e7670ff56dec70e2
Timothy J. Sayre <tsayre@cachehive.com>
2020-07-24 11:30:45 -0700

    match z min endstop and z probe defines

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -655,7 +655,7 @@
 #define X_MAX_ENDSTOP_INVERTING true // set to true to invert the logic of the endstop.
 //#define Y_MAX_ENDSTOP_INVERTING true // set to true to invert the logic of the endstop.
 //#define Z_MAX_ENDSTOP_INVERTING true // set to true to invert the logic of the endstop.
-//#define Z_MIN_PROBE_ENDSTOP_INVERTING false // set to true to invert the logic of the probe.
+#define Z_MIN_PROBE_ENDSTOP_INVERTING true // set to true to invert the logic of the probe.
 
 /**
  * Stepper Drivers
@@ -831,7 +831,7 @@
  *
  * Enable this option for a probe connected to the Z Min endstop pin.
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 /**
  * Z_MIN_PROBE_PIN
